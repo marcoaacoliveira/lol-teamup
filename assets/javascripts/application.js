ceb871d2878b35e162d092f1f5ec4c6b909698f2
Marco Antonio Almeida <marco.almeida@karnovgroup.com>
2013-11-23 14:52:25 +0100

    Remove creators do template.
    Refs #1

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
   $.ajax("views/pages/about.hbs")
   .success(function(data){
     template = Handlebars.compile(data);
-    creators = { creators: ["marco", "marco","hiolanda", "ruan"] }
-    $("main").html(template(creators));
+    $("main").html(template());
   })
   .error(function(data){
     $("main").html(data);
