fadb98427b1f8d8aad27bc9a2bd77306b7e5262c
Marco <marcooliveira@infojr.com.br>
2013-11-28 16:35:39 -0300

    Atualizacao referente ao metodo de edicao de dados. Apenas testando.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
   $.ajax("views/pages/about.hbs")
   .success(function(data){
-    var template = Handlebars.compile(data);
+   	var template = Handlebars.compile(data);
     $("main").html(template());
   })
   .error(function(data){
     $("main").html(data);
   });
+
 });
+
+
+function edit(){
+
+	var username = document.forms["edit"]["username"].value;
+	var summoner = document.forms["edit"]["summoner"].value;
+	var tier = document.forms["edit"]["tier"].value;
+	var role = document.forms["edit"]["role"].value;
+
+
+	console.log("Ola "+username+" bem vindo! Voce eh " +tier+ "na posicao "+role);
+
+}
+
+
+
